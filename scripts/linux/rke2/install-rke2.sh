e1160b2b84fa94a0327c10a65bf570f68e66f75f
marcos.silvestrini <marcos.silvestrini@gmail.com>
2023-07-25 22:27:31 -0300

    Update argocd get password

--- scripts/linux/rke2/install-rke2.sh
+++ scripts/linux/rke2/install-rke2.sh
@@ -249,6 +249,9 @@ function deployments(){
         ## Ingress
         kubectl apply  -f configs/argocd/ingress.yaml
 
+        ## Get password
+        kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d > security/argocd-password
+
         ## Install CLI
         curl -sSL -o argocd-linux-amd64 https://github.com/argoproj/argo-cd/releases/latest/download/argocd-linux-amd64
         install -m 555 argocd-linux-amd64 /usr/local/bin/argocd
