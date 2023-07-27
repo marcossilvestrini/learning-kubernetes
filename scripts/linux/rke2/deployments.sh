9170327639f78fd5e824caf0205de6ef4d6e6c49
marcos.silvestrini <marcos.silvestrini@gmail.com>
2023-07-26 22:33:04 -0300

    Update argocd deployment

--- scripts/linux/rke2/deployments.sh
+++ scripts/linux/rke2/deployments.sh
@@ -47,7 +47,7 @@ function deployments(){
     if [[ "$NODE_MASTER" == *"$NODE_NAME"* ]]; then
         
         # Deploy cert-manager
-
+        echo "DEPLOY CERT-MANAGER STACK"
         ## Add the Helm repository
         helm repo add jetstack https://charts.jetstack.io
         
@@ -63,7 +63,7 @@ function deployments(){
             --set installCRDs=true
 
         # Deploy rancher
-        
+        echo "DEPLOY RANCHER STACK"
         ## Add the Rancher Stable Helm Repo
         helm repo add rancher-stable https://releases.rancher.com/server-charts/stable
         
@@ -77,51 +77,66 @@ function deployments(){
         --set bootstrapPassword=Rancher@123456
         
         # Deploy ArgoCD
+        echo "DEPLOY ARGOCD STACK"
         
         ## Create a namespace
+        echo "CREATE ARGOCD NAMESPACE"
         kubectl create namespace argocd
         
         ## Deployment
+        echo "DEPLOY ARGOCD PODS"
         kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
         
         ## Ingress
+        echo "CREATE ARGOCD INGRESS"
         kubectl apply  -f configs/argocd/ingress.yaml
         
-        ## Get password
-        kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d > security/argocd-password
-        
+        ## Get password        
+        echo "GET ARGOCD INITIAL PASSWORD"
+        #kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d > security/argocd-password
+        ARGO_PASS=$(kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d; echo)
+
         ## Install CLI
+        echo "INSTALL ARGOCD CLI"
         curl -sSL -o argocd-linux-amd64 https://github.com/argoproj/argo-cd/releases/latest/download/argocd-linux-amd64
         install -m 555 argocd-linux-amd64 /usr/local/bin/argocd
         rm argocd-linux-amd64
         
         ## Login in server
+        #--password $(kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d; echo) \
+        echo "LOGIN IN ARGOCD"
         until argocd login argocd.skynet.com.br \
         --username admin \
-        --password $(kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d; echo) \
+        --password "$ARGO_PASS" \
         --insecure --grpc-web; do : ; done
         
         ## Register A Cluster To Deploy Apps To
+        echo "REGISTER CLUSTER TO DEPLOY APPS IN ARGOCD"
         echo "y"|argocd cluster add default
         
-        ## Create An Application From A Git Repository
+        ## Deploy apps from git repository
+        echo "DEPLOY APPS IN ARGOCD FROM GIT REPOSITORY"
         
-        ### First we need to set the current namespace to argocd running the following command:
-        kubectl config set-context --current --namespace=argocd
+        ### Create namespace for apps
+        kubectl create namespace silvestrini
+
+        ### Set default context for namespace silvestrini
+        kubectl config set-context --current --namespace=silvestrini
         
         ### Create the example 1
         argocd app create guestbook \
-        --repo https://github.com/argoproj/argocd-example-apps.git \
-        --path guestbook \
-        --dest-server https://kubernetes.default.svc \
-        --dest-namespace default
+            --repo https://github.com/argoproj/argocd-example-apps.git \
+            --path guestbook \
+            --dest-server https://kubernetes.default.svc \
+            --dest-namespace silvestrini \
+            --insecure
 
         ### Create the example 2 - Helm Charts
         argocd app create helm-guestbook \
             --repo https://github.com/argoproj/argocd-example-apps.git \
             --path helm-guestbook \
             --dest-server https://kubernetes.default.svc \
-            --dest-namespace default    
+            --dest-namespace silvestrini    
 
         ### Create the example 3 - My app - app-silvestrini
         cp -R apps/app-silvestrini/images /mnt/nfs/app-silvestrini
@@ -130,7 +145,17 @@ function deployments(){
             --repo https://github.com/marcossilvestrini/learning-kubernetes.git \
             --path apps/app-silvestrini \
             --dest-server https://kubernetes.default.svc \
-            --dest-namespace default            
+            --dest-namespace silvestrini \
+            --insecure     
+        ### Sync apps
+        echo "SYNC APPS IN ARGOCD"  
+        argocd --insecure app sync app-silvestrini guestbook helm-guestbook
+
+        # ### Update password
+        # argocd account update-password \
+        #     --current-password "$ARGO_PASS" \
+        #     --new-password "Argocd@123456" \
+        #     --insecure
     fi
 }
 
