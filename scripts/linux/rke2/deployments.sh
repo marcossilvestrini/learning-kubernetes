188c7ceb14b6f80f44cbac732f944da08838f724
marcos.silvestrini <marcos.silvestrini@gmail.com>
2023-08-02 09:46:21 -0300

    Update files

--- scripts/linux/rke2/deployments.sh
+++ scripts/linux/rke2/deployments.sh
@@ -90,7 +90,7 @@ function deployments(){
         then
             curl -sSL -o argocd-linux-amd64 https://github.com/argoproj/argo-cd/releases/latest/download/argocd-linux-amd64
             install -m 555 argocd-linux-amd64 /usr/local/bin/argocd
-            rm argocd-linux-amd64                
+            rm argocd-linux-amd64
         fi
         ## Login in server
         echo "LOGIN IN ARGOCD"   
