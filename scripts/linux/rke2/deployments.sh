585f9fcfcd4871e6efa8647692d8a0fec90a6b74
marcos.silvestrini <marcos.silvestrini@gmail.com>
2023-08-16 22:47:49 -0300

    Update files

--- scripts/linux/rke2/deployments.sh
+++ scripts/linux/rke2/deployments.sh
@@ -94,7 +94,7 @@ function deployments() {
         ## install helm chart longhorn
         helm repo add longhorn https://charts.longhorn.io
         helm repo update 
-        helm upgrade --install longhorn longhorn/longhorn \
+        helm upgrade --install longhorn  longhorn/longhorn \
             --namespace longhorn-system \
             --create-namespace \
             --values configs/longhorn/values.yaml
